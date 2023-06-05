0718d5435ab57f6d6a08e6676f1743654218c63e
Jerry Chen <jhinresh@JerrydeMacBook-Pro.local>
2023-06-06 04:21:35 +0800

    about.hero

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-tilt';
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <p>{title}</p>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+
+    <motion.p 
+      variants={fadeIn("", "", 0.1,1)}
+      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+    >
+      I'm a skilled software developer with few experiences in Javascript, and familiar
+       with some frameworks, such Reacr, Node.js, and Three.js. Currently, learning Solidity.
+       I'm a quick learner, and willing to challenge! I hope I can create more applications
+        and cool stuffs in the future.
+    </motion.p>
+    
+    <div className='mt-20 flex flex-wrap gap-10'>
+      {services.map((service, index) => (
+        <ServiceCard key={service.title} index={index} {...service} />
+      ))}
+
+    </div>
+
+    </>
+
+    );
+};
 
 export default About
